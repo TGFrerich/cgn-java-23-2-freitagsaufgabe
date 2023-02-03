3b1f3a555535e8260729475776681ac280f1ba91
timlenny <schlotter.tim@gmail.com>
2023-02-03 11:15:18 +0100

    Test Klassen für Order* Klassen

--- src/test/java/OrderRepoTest.java
+++ src/test/java/OrderRepoTest.java
@@ -1,2 +1,24 @@
+import org.junit.jupiter.api.Test;
+
 public class OrderRepoTest {
+
+    @Test
+    public void isListOutputCorrect(){
+      OrderRepo orders = new OrderRepo();
+      orders.add();
+      List<Orders> = orders.list();
+
+    }
+
+    @Test
+    public void isOrderRepoGetCorrect(){
+
+    }
+
+    @Test
+    public void isOrderRepoAddCorrect(){
+
+    }
+
+
 }
