1ee90d3fffdf86a1f2556318c42864f668f4cc36
hamideh <hamideh_h61@yahoo.com>
2023-02-03 14:13:39 +0100

    created ProductRepoTest

---
+++ src/test/java/org/example/database/ProductRepoTest.java
@@ -0,0 +1,17 @@
+package org.example.database;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepoTest {
+
+    @Test
+    void get() {
+
+    }
+
+    @Test
+    void list() {
+    }
+}
