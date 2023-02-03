47d5c504db52a9e3f785c2e753a45bd38ad92cf9
TG Frerich <themuppet.coder@gmail.com>
2023-02-03 11:06:57 +0100

    add methods in ProductRepo

--- src/main/java/org/example/database/ProductRepo.java
+++ src/main/java/org/example/database/ProductRepo.java
@@ -9,5 +9,11 @@ public class ProductRepo {
 
     List<Product> products = new ArrayList<>();
 
+    public List<Product> get() {
+        return products;
+    }
 
+    public void list(List<Product> products) {
+        this.products = products;
+    }
 }
