9e7ac5516ed70960cbdfd840cb302a7dcf5e5a3a
TG Frerich <themuppet.coder@gmail.com>
2023-02-03 10:52:06 +0100

    setup ProductRepo without methods

--- src/main/java/org/example/database/ProductRepo.java
+++ src/main/java/org/example/database/ProductRepo.java
@@ -1,4 +1,13 @@
 package org.example.database;
 
+import org.example.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductRepo {
+
+    List<Product> products = new ArrayList<>();
+
+
 }
